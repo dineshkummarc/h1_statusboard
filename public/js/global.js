3e6f6fe46d08fb5e0188a7ee731e620215630261
Gareth Walters <g@rethwalte.rs>
2011-07-01 12:36:50 -0700

    animating

--- public/js/global.js
+++ public/js/global.js
@@ -4,21 +4,30 @@ $(function () {
 
 function onMessage(message, author) {
 
-	$('#content').prepend('<div class="message">' + message + '</div>');
+	$('#content').prepend('<div class="message"><div class="title">' + message + '</div><div class="author">' + author + '</div></div>');
+	$('#content').css('margin-top', -138);
 	sizeMessages();
 
 }
 
 function sizeMessages() {
+	
+	$('#content').animate({
+		marginTop: 50
+	}, 2000);
 
 	$('.message').each(function(i){
 
 		var size = 90 / (i+1);
 
-		$(this).css({
-			'font-size' : size + 'px',
-			'line-height' : (size + 50) + 'px'
-		});
+		$(this).children('.title').animate({
+			fontSize: size + 'px',
+			lineHeight: (size + 20) + 'px'
+		}, 2000);
+		
+		if (i > 10) {
+			$(this).remove();
+		}
 
 	});
 
