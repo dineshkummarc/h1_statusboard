b5cafc7a763cbe31e487ebf317b90b3b504b85dc
Brendan Loudermilk <brendan@apwit.com>
2011-07-01 15:50:43 -0700

    Show from

--- server.js
+++ server.js
@@ -52,8 +52,12 @@ app.post('/sms/incoming', function (req, res) {
 });
 
 
+var froms = {};
+
 
 app.post('/voice/incoming', function (req, res) {
+
+  froms[req.body.CallSid] = req.body.From;
 
   var twiml = '<?xml version="1.0" encoding="UTF-8" ?>\n<Response>\n<Say>Please record your message</Say>\n<Record action="' + HOST + '/voice/record" /><Say>Please hang up and try again</Say></Response>';
   res.send(twiml, {'Content-Type':'text/xml'}, 200);
@@ -64,7 +68,7 @@ app.post('/voice/incoming', function (req, res) {
 app.post('/voice/record', function (req, res) {
 
   var url = req.body.RecordingUrl;
-  var from = req.body.From;
+  var from = froms[req.body.CallSid];
   sys.log(url, from);
 
   var twiml = '<?xml version="1.0" encoding="UTF-8" ?>\n<Response>\n<Say>Thanks. Your recording will be played to the office now.</Say>\n<Hangup/>\n</Response>';
