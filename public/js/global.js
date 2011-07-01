572d5576b16095cea0c28fbef8ffebaae15dc256
Brendan Loudermilk <brendan@apwit.com>
2011-07-01 12:12:18 -0700

    Updates

--- public/js/global.js
+++ public/js/global.js
@@ -2,17 +2,24 @@ $(function () {
 	sizeMessages();
 });
 
-function onMessage(message) {
+function onMessage(message, author) {
+
 	$('#content').prepend('<div class="message">' + message + '</div>');
 	sizeMessages();
+
 }
 
 function sizeMessages() {
+
 	$('.message').each(function(i){
-		size = 90 / (i+1);
+
+		var size = 90 / (i+1);
+
 		$(this).css({
 			'font-size' : size + 'px',
 			'line-height' : (size + 50) + 'px'
 		});
+
 	});
+
 }
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+//  Packages
+
 var express   = require('express'),
     app       = express.createServer(),
     nowjs     = require('now'),
     everyone  = nowjs.initialize(app);
 
-var PORT = process.env.PORT || 3000;
+//  Configs
+
+var PORT = process.env.PORT || 3000,
+    TWILIO_SID = "ACa9ac2b83e4340ae8bbc6a38bd8a35376",
+    TWILIO_TOKEN = "b0ec1cdf09ab21517fd39cef12bffb86",
+    TWILIO_HOST = "http://severe-lightning-442.herokuapp.com/",
+    TWILIO_NUM = "+18052146633";
 
 /////////////
 //         //
@@ -29,3 +37,15 @@ app.listen(PORT);
 /////////
 
 
+//  Twilio  //
+
+var TwilioClient = require('twilio/client'),
+    client = new TwilioClient(sid, authToken, hostname, opts);
+
+var phoneNumber = getPhoneNumber(TWILIO_NUM);
+
+phoneNumber.on('incomingSms', function (smsParams, response) {
+
+  console.log(smsParams, response);
+
+});
