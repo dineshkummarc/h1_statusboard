f7b0187539d58ad19307d83d7b4d901ad9b89d79
Gareth Walters <g@rethwalte.rs>
2011-07-01 12:01:35 -0700

    styling

--- public/js/global.js
+++ public/js/global.js
@@ -1,8 +1,18 @@
 $(function () {
-
-  console.log('test');
-
+	sizeMessages();
 });
 
 function onMessage(message) {
+	$('#content').prepend('<div class="message">' + message + '</div>');
+	sizeMessages();
+}
+
+function sizeMessages() {
+	$('.message').each(function(i){
+		size = 90 / (i+1);
+		$(this).css({
+			'font-size' : size + 'px',
+			'line-height' : (size + 50) + 'px'
+		});
+	});
 }
